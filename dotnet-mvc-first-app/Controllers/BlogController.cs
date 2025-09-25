97ca0f7671f90b55c53cf97f94616a3f4e655e38
HP <HP@ORPHINE>
2025-09-25 15:55:06 +0700

    Add project files.

---
+++ dotnet-mvc-first-app/Controllers/BlogController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_mvc_first_app.Controllers
+{
+    public class BlogController : Controller
+    {
+        [Route("my-blog")]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
