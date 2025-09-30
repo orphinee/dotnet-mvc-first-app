915d07fb875cabfd922c8e64f7842e820035c44c
orphinee <zhafiraql@gmail.com>
2025-09-30 21:18:51 +0700

    commit all

---
+++ dotnet-mvc-first-app/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_mvc_first_app.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /Account/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            // Contoh validasi sederhana (belum pakai database)
+            if (username == "admin" && password == "123")
+            {
+                // redirect ke Dashboard setelah login sukses
+                return RedirectToAction("Index", "Dashboard");
+            }
+
+            // kalau login gagal
+            ViewBag.Error = "Username atau password salah!";
+            return View("Index");
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
---
+++ dotnet-mvc-first-app/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_mvc_first_app.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Dashboard";
+            return View();
+        }
+    }
+}
